fb8079bca32094efec81dc58d0744a7f7c533e48
manepullavamsi1998 <manepullavamsi1998@gmail.com>
2023-05-06 16:35:27 +0530

    project intial configuration.

---
+++ farmer-milk/src/main/java/com/farmer/milk/FarmerMilkApplication.java
@@ -0,0 +1,13 @@
+package com.farmer.milk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerMilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerMilkApplication.class, args);
+	}
+
+}
---
+++ farmer-milk/src/test/java/com/farmer/milk/FarmerMilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmer.milk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerMilkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
